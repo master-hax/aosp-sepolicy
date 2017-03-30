7f231c06f8e6318b33e7966ab565ac586feb1b25
Dan Cashman <dcashman@google.com>
2017-03-30 14:51:51 -0700

    Add reverse-attribute mapping to sepolicy-analyze.
    sepolicy-analyze allows users to see all types that have a given
    attribute, but not the reverse case: all attributes of a given type.
    Add a '--reverse' option which enables this, but keeps the previous
    interface.
    
    Bug: 36508258
    Test: Build and run against current policy.
    Change-Id: Ice6893cf7aa2ec4706a7411645a8e0a8a3ad01eb

--- tools/sepolicy-analyze/attribute.c
+++ tools/sepolicy-analyze/attribute.c
@@ -1,10 +1,12 @@
+#include <getopt.h>
+
 #include "attribute.h"
 
 void attribute_usage() {
-    fprintf(stderr, "\tattribute <attribute-name>\n");
+    fprintf(stderr, "\tattribute <name> [-r|--reverse]\n");
 }
 
-static int list_attribute(policydb_t * policydb, char *name)
+static int list_attribute(policydb_t * policydb, char *name, int reverse)
 {
     struct type_datum *attr;
     struct ebitmap_node *n;
@@ -16,24 +18,55 @@ static int list_attribute(policydb_t * policydb, char *name)
         return -1;
     }
 
-    if (attr->flavor != TYPE_ATTRIB) {
-        fprintf(stderr, "%s is a type not an attribute in this policy.\n", name);
-        return -1;
-    }
-
-    ebitmap_for_each_bit(&policydb->attr_type_map[attr->s.value - 1], n, bit) {
-        if (!ebitmap_node_get_bit(n, bit))
-            continue;
-        printf("%s\n", policydb->p_type_val_to_name[bit]);
+    if (reverse) {
+        if (attr->flavor != TYPE_TYPE) {
+            fprintf(stderr, "%s is an attribute not a type in this policy.\n", name);
+            return -1;
+        }
+        ebitmap_for_each_bit(&policydb->type_attr_map[attr->s.value - 1], n, bit) {
+            if (!ebitmap_node_get_bit(n, bit))
+                continue;
+            printf("%s\n", policydb->p_type_val_to_name[bit]);
+        }
+    } else {
+        if (attr->flavor != TYPE_ATTRIB) {
+            fprintf(stderr, "%s is a type not an attribute in this policy.\n", name);
+            return -1;
+        }
+        ebitmap_for_each_bit(&policydb->attr_type_map[attr->s.value - 1], n, bit) {
+            if (!ebitmap_node_get_bit(n, bit))
+                continue;
+            printf("%s\n", policydb->p_type_val_to_name[bit]);
+        }
     }
 
     return 0;
 }
 
 int attribute_func (int argc, char **argv, policydb_t *policydb) {
-    if (argc != 2) {
+    int reverse = 0;
+    char *name = argv[1];
+    char ch;
+
+    struct option attribute_options[] = {
+        {"reverse", no_argument, NULL, 'r'},
+        {NULL, 0, NULL, 0}
+    };
+
+    while ((ch = getopt_long(argc - 1, argv + 1, "r", attribute_options, NULL)) != -1) {
+        switch (ch) {
+        case 'r':
+            reverse = 1;
+            break;
+        default:
+            USAGE_ERROR = true;
+            return -1;
+        }
+    }
+
+    if (argc != 2 && !(reverse && argc == 3)) {
         USAGE_ERROR = true;
         return -1;
     }
-    return list_attribute(policydb, argv[1]);
+    return list_attribute(policydb, name, reverse);
 }
