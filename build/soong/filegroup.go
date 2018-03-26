e75bce82315457cacd849074f51209b2f4e10775
Tri Vo <trong@google.com>
2018-03-25 20:03:58 -0700

    Soong module of selinux files in board-specific directories.
    se_filegroup is used to export selinux files from board-specific
    directories (e.g. device/google/wahoo/sepolicy).
    
    Use se_filegroup module to export partner extension of compatibility
    mapping to build logic in system/sepolicy.
    
    Bug: 33691272
    Bug: 74669108
    Test: .cil mapping files can be correctly added from vendor directory.
    Change-Id: Iaa2a95d0e326cb03a478fc12c1a14ba40e57e117

---
+++ build/soong/filegroup.go
@@ -0,0 +1,86 @@
+// Copyright 2018 Google Inc. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package selinux
+
+import (
+	"android/soong/android"
+	"path/filepath"
+)
+
+func init() {
+	android.RegisterModuleType("se_filegroup", FileGroupFactory)
+}
+
+func FileGroupFactory() android.Module {
+	module := &fileGroup{}
+	module.AddProperties(&module.properties)
+	android.InitAndroidModule(module)
+	return module
+}
+
+type fileGroupProperties struct {
+	Srcs    []string
+	SrcDirs []string
+}
+
+type fileGroup struct {
+	android.ModuleBase
+	properties fileGroupProperties
+	srcs       android.Paths
+}
+
+var _ android.SourceFileProducer = (*fileGroup)(nil)
+
+func (fg *fileGroup) Srcs() android.Paths {
+	return fg.srcs
+}
+
+func (fg *fileGroup) DepsMutator(ctx android.BottomUpMutatorContext) {
+	android.ExtractSourcesDeps(ctx, fg.properties.Srcs)
+}
+
+func (fg *fileGroup) GenerateAndroidBuildActions(ctx android.ModuleContext) {
+	productVars := ctx.Config().ProductVariables
+	srcDirsMap := map[string][]string{
+		"BOARD_PLAT_PUBLIC_SEPOLICY_DIR": []string{
+			productVars.BoardPlatPrivateSepolicyDir},
+		"BOARD_PLAT_PRIVATE_SEPOLICY_DIR": []string{
+			productVars.BoardPlatPrivateSepolicyDir},
+		"BOARD_SEPOLICY_DIRS":     productVars.BoardVendorSepolicyDirs,
+		"BOARD_ODM_SEPOLICY_DIRS": productVars.BoardOdmSepolicyDirs,
+	}
+
+	var allSrcDirs []string
+	for _, d := range fg.properties.SrcDirs {
+		boardDirs, ok := srcDirsMap[d]
+		if !ok {
+			ctx.ModuleErrorf("se_filegroup is only for files in sepolicy directories.")
+		}
+		allSrcDirs = append(allSrcDirs, boardDirs...)
+	}
+
+	for _, d := range allSrcDirs {
+		for _, f := range fg.properties.Srcs {
+			path := filepath.Join(d, f)
+			files, err := ctx.GlobWithDeps(path, nil)
+			if err != nil {
+				ctx.ModuleErrorf("glob: %s", err.Error())
+			}
+			for _, f := range files {
+				fg.srcs = append(fg.srcs, android.PathForSource(ctx, f))
+			}
+		}
+	}
+}
