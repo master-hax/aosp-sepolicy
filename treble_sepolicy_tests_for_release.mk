232799624468a54de28cfc2e294b221378128a09
Tri Vo <trong@google.com>
2018-09-29 17:47:10 -0700

    Only maintain maps between current and previous selinux versions.
    New maintenance scheme for mapping files:
    Say, V is the current SELinux platform version, then at any point in time we
    only maintain (V->V-1) mapping. (V->V-n) map is constructed from top (V->V-n+1)
    and bottom (V-n+1->V-n) without changes to previously maintained mapping files.
    
    Caveats:
    - 26.0.cil doesn't technically represent 27.0->26.0 map, but rather
    current->26.0. We'll fully migrate to the scheme with future releases.
    
    Bug: 67510052
    Test: adding new public type only requires changing the latest compat map
    Change-Id: Iab5564e887ef2c8004cb493505dd56c6220c61f8

--- treble_sepolicy_tests_for_release.mk
+++ treble_sepolicy_tests_for_release.mk
@@ -51,8 +51,8 @@ $(version)_plat_policy.conf :=
 # targeting the $(version) SELinux release.  This ensures that our policy will build
 # when used on a device that has non-platform policy targetting the $(version) release.
 $(version)_compat := $(intermediates)/$(version)_compat
-$(version)_mapping.cil := $(LOCAL_PATH)/private/compat/$(version)/$(version).cil
-$(version)_mapping.ignore.cil := $(LOCAL_PATH)/private/compat/$(version)/$(version).ignore.cil
+$(version)_mapping.cil := $(TARGET_OUT)/etc/selinux/mapping/$(version).cil
+$(version)_mapping.ignore.cil := $(TARGET_OUT)/etc/selinux/mapping/$(version).ignore.cil
 $(version)_prebuilts_dir := $(LOCAL_PATH)/prebuilts/api/$(version)
 
 # vendor_sepolicy.cil and plat_pub_versioned.cil are the new design to replace
