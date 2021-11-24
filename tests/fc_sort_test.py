be20ce4fb0739eb46cf22e1699d74ddb84fa3e54
Thiébaud Weksteen <tweek@google.com>
2021-11-24 14:44:28 +1100

    Refactor fc_sort and add unit tests
    Clean up fc_sort to facilitate the migration to Python3. Use PEP8 for
    naming scheme.
    
    Test: atest --host fc_sort_test
    Bug: 200119288
    Change-Id: Ia2c40a850a48ec75e995d5233b5abaae10917a89

---
+++ tests/fc_sort_test.py
@@ -0,0 +1,36 @@
+import unittest
+
+import fc_sort
+
+class FcSortTest(unittest.TestCase):
+
+  def testGetStemLen(self):
+    self.assertEqual(fc_sort.get_stem_len("/data"), 5)
+    self.assertEqual(fc_sort.get_stem_len("/data/system"), 12)
+    self.assertEqual(fc_sort.get_stem_len("/data/(system)?"), 6)
+
+  def testIsMeta(self):
+    self.assertEqual(fc_sort.is_meta("/data"), False)
+    self.assertEqual(fc_sort.is_meta("/data$"), True)
+    self.assertEqual(fc_sort.is_meta("\$data"), False)
+
+  def testReadFileContexts(self):
+    content = """# comment
+/                   u:object_r:rootfs:s0
+# another comment
+/adb_keys           u:object_r:adb_keys_file:s0
+"""
+    fcs = fc_sort.read_file_contexts(content.splitlines())
+    self.assertEqual(len(fcs), 2)
+
+    self.assertEqual(fcs[0].path, "/")
+    self.assertEqual(fcs[0].type, "rootfs")
+
+    self.assertEqual(fcs[1].path, "/adb_keys")
+    self.assertEqual(fcs[1].type, "adb_keys_file")
+
+if __name__ == '__main__':
+  #suite = unittest.TestLoader().loadTestsFromName(__name__)
+  #unittest.TextTestRunner(verbosity=2).run(suite)
+  unittest.main(verbosity=2)
+
