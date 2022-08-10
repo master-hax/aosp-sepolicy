d6904acf4242af79dc494d4f8bda1326ece003a8
Myles Watson <mylesgw@google.com>
2022-08-10 14:19:27 -0700

    sepolicy: Add Bluetooth AIDL
    Bug: 205758693
    Test: manual - boot local image with Cuttlefish
    Change-Id: Ic0c5408d83f8c352b72f79e9024212c7ff0c84c1

--- build/soong/service_fuzzer_bindings.go
+++ build/soong/service_fuzzer_bindings.go
@@ -29,6 +29,7 @@ var (
 		"android.hardware.biometrics.fingerprint.IFingerprint/default":            []string{},
 		"android.hardware.biometrics.fingerprint.IFingerprint/virtual":            []string{},
 		"android.hardware.bluetooth.audio.IBluetoothAudioProviderFactory/default": []string{},
+		"android.hardware.bluetooth.IBluetoothHci/default":                        []string{},
 		"android.hardware.camera.provider.ICameraProvider/internal/0":             []string{},
 		"android.hardware.contexthub.IContextHub/default":                         []string{},
 		"android.hardware.drm.IDrmFactory/clearkey":                               []string{},
