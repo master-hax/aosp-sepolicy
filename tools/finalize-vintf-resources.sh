033c480ac0df6df3123e2f22a0d0a43fd194fad1
Inseob Kim <inseob@google.com>
2024-07-19 18:21:18 +0900

    Update vintf finalization script for flagging
    Bug: 353121021
    Test: manual
    Change-Id: I0dd4e2c332229f4bc0e0d95fa8ba5d3752d2fe70

--- tools/finalize-vintf-resources.sh
+++ tools/finalize-vintf-resources.sh
@@ -29,70 +29,109 @@ cp -r "$top/system/sepolicy/private/" "$top/system/sepolicy/prebuilts/api/${ver}
 cat > "$top/system/sepolicy/prebuilts/api/${ver}/Android.bp" <<EOF
 // Automatically generated file, do not edit!
 se_policy_conf {
-    name: "${ver}_plat_pub_policy.conf",
+    name: "{ver}_reqd_policy_mask.conf",
     defaults: ["se_policy_conf_flags_defaults"],
-    srcs: [":se_build_files{.plat_public_${ver}}", ":se_build_files{.reqd_mask}"],
+    srcs: reqd_mask_policy,
     installable: false,
     build_variant: "user",
+    board_api_level: "{ver}",
 }
 
 se_policy_cil {
-    name: "${ver}_plat_pub_policy.cil",
-    src: ":${ver}_plat_pub_policy.conf",
-    filter_out: [":reqd_policy_mask.cil"],
+    name: "{ver}_reqd_policy_mask.cil",
+    src: ":{ver}_reqd_policy_mask.conf",
     secilc_check: false,
     installable: false,
 }
 
 se_policy_conf {
-    name: "${ver}_product_pub_policy.conf",
+    name: "{ver}_plat_pub_policy.conf",
     defaults: ["se_policy_conf_flags_defaults"],
     srcs: [
-        ":se_build_files{.plat_public_${ver}}",
-        ":se_build_files{.system_ext_public_${ver}}",
-        ":se_build_files{.product_public_${ver}}",
+        ":se_build_files{.plat_public_{ver}}",
         ":se_build_files{.reqd_mask}",
     ],
     installable: false,
     build_variant: "user",
+    board_api_level: "{ver}",
 }
 
 se_policy_cil {
-    name: "${ver}_product_pub_policy.cil",
-    src: ":${ver}_product_pub_policy.conf",
-    filter_out: [":reqd_policy_mask.cil"],
+    name: "{ver}_plat_pub_policy.cil",
+    src: ":{ver}_plat_pub_policy.conf",
+    filter_out: [":{ver}_reqd_policy_mask.cil"],
     secilc_check: false,
     installable: false,
 }
 
 se_policy_conf {
-    name: "${ver}_plat_policy.conf",
+    name: "{ver}_product_pub_policy.conf",
     defaults: ["se_policy_conf_flags_defaults"],
     srcs: [
-        ":se_build_files{.plat_public_${ver}}",
-        ":se_build_files{.plat_private_${ver}}",
-        ":se_build_files{.system_ext_public_${ver}}",
-        ":se_build_files{.system_ext_private_${ver}}",
-        ":se_build_files{.product_public_${ver}}",
-        ":se_build_files{.product_private_${ver}}",
+        ":se_build_files{.plat_public_{ver}}",
+        ":se_build_files{.system_ext_public_{ver}}",
+        ":se_build_files{.product_public_{ver}}",
+        ":se_build_files{.reqd_mask}",
     ],
     installable: false,
     build_variant: "user",
+    board_api_level: "{ver}",
 }
 
 se_policy_cil {
-    name: "${ver}_plat_policy.cil",
-    src: ":${ver}_plat_policy.conf",
-    additional_cil_files: [":sepolicy_technical_debt{.plat_private_${ver}}"],
+    name: "{ver}_product_pub_policy.cil",
+    src: ":{ver}_product_pub_policy.conf",
+    filter_out: [":{ver}_reqd_policy_mask.cil"],
+    secilc_check: false,
+    installable: false,
+}
+
+se_versioned_policy {
+    name: "{ver}_plat_pub_versioned.cil",
+    base: ":{ver}_product_pub_policy.cil",
+    target_policy: ":{ver}_product_pub_policy.cil",
+    version: "{ver}",
+    installable: false,
+}
+
+se_policy_conf {
+    name: "{ver}_plat_policy.conf",
+    defaults: ["se_policy_conf_flags_defaults"],
+    srcs: [
+        ":se_build_files{.plat_public_{ver}}",
+        ":se_build_files{.plat_private_{ver}}",
+        ":se_build_files{.system_ext_public_{ver}}",
+        ":se_build_files{.system_ext_private_{ver}}",
+        ":se_build_files{.product_public_{ver}}",
+        ":se_build_files{.product_private_{ver}}",
+    ],
+    installable: false,
+    build_variant: "user",
+    board_api_level: "{ver}",
+}
+
+se_policy_cil {
+    name: "{ver}_plat_policy.cil",
+    src: ":{ver}_plat_policy.conf",
+    additional_cil_files: [":sepolicy_technical_debt{.plat_private_{ver}}"],
     installable: false,
 }
 
 se_policy_binary {
-    name: "${ver}_plat_policy",
-    srcs: [":${ver}_plat_policy.cil"],
+    name: "{ver}_plat_policy",
+    srcs: [":{ver}_plat_policy.cil"],
     installable: false,
     dist: {
         targets: ["base-sepolicy-files-for-mapping"],
     },
+}
+
+filegroup {
+    name: "{ver}_sepolicy_cts_data",
+    srcs: [
+        "{ver}_general_sepolicy.conf",
+        "{ver}_plat_sepolicy.cil",
+        "{ver}_mapping.cil",
+    ],
 }
 EOF
